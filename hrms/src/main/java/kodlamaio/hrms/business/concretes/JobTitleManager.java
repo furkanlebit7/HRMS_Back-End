d179daa0eb174d5d9416706d6fb8663e57304999
furkanlebit7 <furkanlebitt7@gmail.com>
2021-05-12 16:36:02 +0300

    first commit :')

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobTitleManager.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.dataAccess.abstracts.JobTitleDao;
+import kodlamaio.hrms.entities.concretes.JobTitles;
+
+@Service
+public class JobTitleManager implements JobTitleService{
+	
+	
+	private JobTitleDao jobTitleDao;
+	
+	@Autowired
+	public JobTitleManager(JobTitleDao jobTitleDao) {
+		super();
+		this.jobTitleDao = jobTitleDao;
+	}
+	
+	@Override
+	public List<JobTitles> getAll() {
+		return jobTitleDao.findAll();
+	}
+
+
+
+	
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitles.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="job_titles")
+public class JobTitles {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="id")
+	private int id;
+
+	@Column(name="title")
+	private String title;
+	
+	
+}
